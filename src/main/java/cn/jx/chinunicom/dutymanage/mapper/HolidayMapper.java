3b3639237cca58f56fd359f627a77e844cc9042b
379805339@qq.com <379805339@qq.com>
2021-01-18 14:27:01 +0800

    修改排班逻辑

--- src/main/java/cn/jx/chinunicom/dutymanage/mapper/HolidayMapper.java
+++ src/main/java/cn/jx/chinunicom/dutymanage/mapper/HolidayMapper.java
@@ -2,6 +2,7 @@ package cn.jx.chinunicom.dutymanage.mapper;
 
 import cn.jx.chinunicom.dutymanage.entity.Holiday;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface HolidayMapper extends BaseMapper<Holiday> {
     List<Date> getHoliday_in_dutyScope(Date begin,Date end);
     List<Date> getHolidayByHolidayType(int holidayType);
+    List<Date> getHolidayByTypeAndYear(@Param("HolidayType") int holidayType,@Param("year") String year);
 }
